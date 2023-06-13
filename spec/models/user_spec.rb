d53275bfc2f686c5d12632eb4a63c00e57cf6b21
KellenCheng <kellen@shoplineapp.com>
2023-06-13 17:19:42 +0800

    test(user): add model simple test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    model = described_class.new(name: "user_001")
+    expect(model).to be_valid
+  end
+
+  it "has many sleep_records" do
+    association = described_class.reflect_on_association(:sleep_records)
+    expect(association.macro).to eq(:has_many)
+  end
+
+  it "has many subscriptions" do
+    association = described_class.reflect_on_association(:subscriptions)
+    expect(association.macro).to eq(:has_many)
+  end
 end
