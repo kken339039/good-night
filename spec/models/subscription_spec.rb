5631f40ab78ce7c44d9a2995005bfde25e526954
KellenCheng <kellen@shoplineapp.com>
2023-06-16 15:14:32 +0800

    feat(subscription): add simple model test

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:target_user) { create(:user) }
+
+  it "is valid with valid attributes" do
+    model = described_class.new(
+      user_id: user.id,
+      target_user_id: target_user.id,
+      actived_at: DateTime.now
+    )
+    expect(model).to be_valid
+  end
+
+   it "belongs to user" do
+    association = described_class.reflect_on_association(:user)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+   it "belongs to target_user" do
+    association = described_class.reflect_on_association(:target_user)
+    expect(association.macro).to eq(:belongs_to)
+  end
 end
