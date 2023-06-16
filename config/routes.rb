81d890127752db8583d18e9174d86fb08383a40b
KellenCheng <kellen@shoplineapp.com>
2023-06-16 16:35:27 +0800

    feat(route): init sleep check in & out action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, path: '/api/:user_id' do
+    resources :sleep_records, only: [], controller: 'sleep_records' do
+      collection do
+        post :check_in
+        post :check_out
+      end
+    end
+  end
 end
