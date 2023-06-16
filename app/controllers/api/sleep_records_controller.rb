b79878f60c4c06d58e3665dcb2c1985a526a0997
KellenCheng <kellen@shoplineapp.com>
2023-06-16 18:10:21 +0800

    feat(sleep_record): add status field and validate

--- app/controllers/api/sleep_records_controller.rb
+++ app/controllers/api/sleep_records_controller.rb
@@ -2,7 +2,7 @@ class Api::SleepRecordsController < ApplicationController
   before_action :set_current_user
 
   def check_in
-    check_in_record = SleepRecord.create!(user_id: current_user.id, check_in_at: Time.now)
+    check_in_record = SleepRecord.create!(user_id: current_user.id, check_in_at: Time.now, status: 'active')
     render json: { result: "success" }, status: :ok
   rescue => ex
     Rails.logger.error(ex.to_s)
--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -5,7 +5,15 @@ class SleepRecord < ApplicationRecord
   # attributes
   attribute :check_in_at, :datetime
   attribute :check_out_at, :datetime
+  attribute :status, :string, default: 'active'
   attribute :duration, :float, default: 0
 
   # validation
+  validates_inclusion_of :status, in: %w(active removed check_out), message: "status %{value} is not valid"
+  validate :valid_only_one_active
+
+  def valid_only_one_active
+    active_records = SleepRecord.where(user_id: self.user_id, status: 'active')
+    errors.add(:status, "active record can only exist at a time") if active_records.present?
+  end
 end
