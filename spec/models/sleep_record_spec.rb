42ee0e5a8b6159a9f04c3e443b5e73caff08e53c
KellenCheng <kellen@shoplineapp.com>
2023-06-16 15:01:52 +0800

    test(sleep_record): add model simple test

--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SleepRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  it "is valid with valid attributes" do
+    model = described_class.new(
+      user_id: user.id,
+      check_in_at: DateTime.now,
+      check_out_at: DateTime.now + 1.days,
+      duration: 100.01
+    )
+    expect(model).to be_valid
+  end
+
+  it "belongs to user" do
+    association = described_class.reflect_on_association(:user)
+    expect(association.macro).to eq(:belongs_to)
+  end
 end
