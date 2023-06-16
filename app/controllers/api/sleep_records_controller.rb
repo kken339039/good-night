16615c513b52efa624b5310412963ba861a319c6
KellenCheng <kellen@shoplineapp.com>
2023-06-16 18:41:11 +0800

    feat(check-out): add check out action

--- app/controllers/api/sleep_records_controller.rb
+++ app/controllers/api/sleep_records_controller.rb
@@ -10,6 +10,30 @@ class Api::SleepRecordsController < ApplicationController
     render json: error.as_json, status: error.status
   end
 
+  def check_out
+    check_in_record = SleepRecord.find_by(user_id: current_user.id, check_out_at: nil)
+    raise CheckInRecordNotFoundError.new("Cannot Found check in record") if check_in_record.nil?
+
+    check_out_at = Time.now
+    duration = check_out_at - check_in_record.check_in_at
+    check_in_record.update!(check_out_at: check_out_at, duration: duration, status: 'check_out')
+
+    render json: { result: "success" }, status: :ok
+  rescue CheckInRecordNotFoundError => ex
+    Rails.logger.error(ex.to_s)
+    render json: ex.as_json, status: ex.status
+  rescue => ex
+    Rails.logger.error(ex.to_s)
+    error = SleepRecordsControllerError.new(ex.to_s)
+    render json: error.as_json, status: error.status
+  end
+
+  class CheckInRecordNotFoundError < ApplicationError
+    def status
+      :not_found
+    end
+  end
+
   class SleepRecordsControllerError < ApplicationError
     def status
       :unprocessable_entity
