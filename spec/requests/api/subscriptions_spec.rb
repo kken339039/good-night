23c7f9dedf2ac9f11fb543a8244be63872aec179
KellenCheng <kellen@shoplineapp.com>
2023-06-18 15:52:21 +0800

    feat(subscription): add index action spec

--- spec/requests/api/subscriptions_spec.rb
+++ spec/requests/api/subscriptions_spec.rb
@@ -1,6 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Subscriptions", type: :request do
+  describe "GET index" do
+    let!(:user1){ create(:user, name: "user1") }
+    let!(:user2){ create(:user, name: "user2") }
+    let!(:user3){ create(:user, name: "user3") }
+
+    let!(:subscription1){ create(:subscription, user_id: user1.id, target_user_id: user2.id) }
+    let!(:subscription2){ create(:subscription, user_id: user1.id, target_user_id: user3.id) }
+
+    let!(:sleep_record1){ create(:sleep_record, user_id: user2.id, status: 'check_out', check_in_at: (Time.now - rand(0..7).days), check_out_at: Time.now, duration: 5000) }
+    let!(:sleep_record2){ create(:sleep_record, user_id: user2.id, status: 'check_out', check_in_at: (Time.now - rand(0..7).days), check_out_at: Time.now, duration: 6000) }
+    let!(:sleep_record3){ create(:sleep_record, user_id: user2.id, status: 'check_out', check_in_at: (Time.now - rand(0..7).days), check_out_at: Time.now, duration: 7000) }
+    let!(:sleep_record4){ create(:sleep_record, user_id: user3.id, status: 'check_out', check_in_at: (Time.now - rand(0..7).days), check_out_at: Time.now, duration: 15000) }
+    let!(:sleep_record5){ create(:sleep_record, user_id: user3.id, status: 'check_out', check_in_at: (Time.now - rand(0..7).days), check_out_at: Time.now, duration: 20000) }
+    let!(:sleep_record6){ create(:sleep_record, user_id: user3.id, status: 'check_out', check_in_at: (Time.now - rand(0..7).days), check_out_at: Time.now, duration: 25000) }
+
+    it "success get subscriptions" do
+      get "/api/#{user1.id}/subscriptions"
+
+      response_body = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(response_body.first[0]).to eq(user3.name)
+    end
+
+    context 'when user not have subscription' do
+      it "success get empty subscriptions" do
+        get "/api/#{user3.id}/subscriptions"
+
+        expect(response).to have_http_status(200)
+        expect(response.body).to eq("{}")
+      end
+    end
+
+    context 'when record all over 7 days ago' do
+      let!(:user4){ create(:user, name: "user4") }
+      let!(:subscription2){ create(:subscription, user_id: user2.id, target_user_id: user4.id) }
+
+      let!(:not_range_record){ create(:sleep_record, user_id: user4.id, status: 'check_out', check_in_at: (Time.now - 8.days), check_out_at: Time.now, duration: 5000) }
+
+      it "success get empty subscriptions" do
+        get "/api/#{user2.id}/subscriptions"
+
+        expect(response).to have_http_status(200)
+        expect(response.body).to eq("{}")
+      end
+    end
+  end
+
   describe "POST activate" do
     let(:user){ create(:user) }
     let(:target_user){ create(:user) }
