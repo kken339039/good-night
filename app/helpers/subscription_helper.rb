d9620161f1cbf6ec4d23507894d1a9d97ecabcce
KellenCheng <kellen@shoplineapp.com>
2023-06-18 12:58:58 +0800

    fix(render): fix render format

--- app/helpers/subscription_helper.rb
+++ app/helpers/subscription_helper.rb
@@ -1,5 +1,11 @@
 module SubscriptionHelper
   def render_sorted_records(sleep_records)
-    sleep_records.sort_by { |_, total| -total }.to_h
+    sleep_records = sleep_records.sort_by { |_, total| -total }.to_h
+    sleep_records.each_with_object({}) do |(name, total_seconds), hash|
+      hours = total_seconds / 3600
+      minutes = (total_seconds % 3600) / 60
+      seconds = total_seconds % 60
+      hash[name] = "#{hours.to_i}hours #{minutes.to_i}minutes #{seconds.to_i}seconds"
+    end
   end
 end
